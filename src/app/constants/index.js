b8847e6220805a44bb8193beef1c94b897600e96
renvelvet <reshapuspita734@gmail.com>
2023-06-24 16:59:09 +0700

    init project

---
+++ src/app/constants/index.js
@@ -0,0 +1,16 @@
+const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'work',
+    title: 'Work',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+]
+
+export { navLinks }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#051027',
+        secondary: '#aaa6c3',
+        tertiary: '#151030',
+        'pink-squid-game': '#FD7293',
+        'blue-squid-game': '#6CA4D5',
+      },
+      boxShadow: {
+        card: '0px 0px 70px -15px #211e35',
+        header: '0px 0px 70px -15px #FD7293',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        'bounce-slow': 'ease infinite alternate 2s',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 }
